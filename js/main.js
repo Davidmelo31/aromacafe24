0891da45724f8326f8f9849821b65b6bdb769bcd
Davidmelo31 <davidmelo89@icloud.com>
2025-07-01 13:34:21 -0500

    LOGO
    DS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const botones = document.querySelectorAll('.categoria');
-  botones.forEach(btn => {
-    btn.addEventListener('click', () => {
-      const contenido = btn.nextElementSibling;
-      contenido.style.display = (contenido.style.display === 'block') ? 'none' : 'block';
-    });
+const botones = document.querySelectorAll(".categoria");
+
+botones.forEach(boton => {
+  boton.addEventListener("click", () => {
+    const contenido = boton.nextElementSibling;
+    contenido.style.display = contenido.style.display === "block" ? "none" : "block";
   });
 });
